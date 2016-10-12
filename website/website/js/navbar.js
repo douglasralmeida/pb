19f07e7410da7ba66587c8f0732366246fc289b2
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2016-10-12 17:32:53 -0300

    Novos artigos no banco de conhecimentos e página de atalhos

---
+++ website/website/js/navbar.js
@@ -0,0 +1,11 @@
+/*!
+ * Barra de Navegacao do Portal da BHSul
+ * Douglas Rodrigues de Almeida
+ * douglasralmeida@live.com
+ */
+
+jQuery(document).ready(function($){
+    "use strict";
+
+    $("<div/>").load("widgets/navbar.html").insertBefore($(".conteudo-pagina"));
+});
