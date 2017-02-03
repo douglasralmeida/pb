4da20572d4f3179dbdec60b81734453a94cc127a
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2017-02-03 02:13:26 -0200

    Página de Gestão de Feedbacks

--- README.md
+++ README.md
@@ -8,10 +8,10 @@ Angular JS 1.5.8 - https://angularjs.org/
 Metro UI CSS 3.0.15 - http://metroui.org.ua/
 
 Para implantar:
-        docker run --name portalbhsul -d -p 80:80 --restart unless-stopped douglasralmeida/portalbhsul
+    docker run --name portalbhsul -d -p 80:80 --restart unless-stopped douglasralmeida/portalbhsul
 
 Para atualizar:
-        docker pull douglasralmeida/portalbhsul
-        docker stop portalbhsul
-        docker rm portalbhsul
-        docker run --name portalbhsul -d -p 80:80 --restart unless-stopped douglasralmeida/portalbhsul
+    docker pull douglasralmeida/portalbhsul
+    docker stop portalbhsul
+    docker rm portalbhsul
+    docker run --name portalbhsul -d -p 80:80 --restart unless-stopped douglasralmeida/portalbhsul
--- website/app/feedback/js/feedback.js
+++ website/app/feedback/js/feedback.js
@@ -2,12 +2,23 @@
  * Gestão de Feedback do Portal da BHSul
  * Douglas Rodrigues de Almeida
  * douglasralmeida@live.com
- */		
+ */
+ 
+
+
 var app = angular.module('GestaoFeedbackApp', []);
  
 app.controller('feedbacksCtrl', function($scope, $http) {
-    $http.get('http://' + location.host + ':8080')
-    .then(function (response) {$scope.feedbacks = response.data;});
+    //$http.get('http://' + location.host + ':8080')
+    //.then(function (response) {$scope.feedbacks = response.data;});
+	
+	$scope.feedbacks = [{"id":1,"tipo":1,"descricao":"Um teste"},{"id":2,"tipo":2,"descricao":"Outro teste"}];
+});
+
+$("lv1").listview({
+    onListClick: function(_list){
+        alert(list.attr('id'));
+}
 });
 
 /*angular.element(document).ready(function() {
