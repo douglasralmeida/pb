a799bfe185a80c32d307140597384b6e3c03033e
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2017-08-26 18:07:13 -0300

    Parte JS transferida para um arquivo próprio na página inicial e de atalhos

---
+++ website/js/bhsul-0.0.1.js
@@ -0,0 +1,42 @@
+/*!
+ * Portal da BH Sul
+ * Douglas Rodrigues de Almeida
+ * douglasralmeida@live.com
+ */
+
+var app = angular.module('PortalBHSul', []);
+
+app.controller('paginicialCtrl',  function($scope, $http) {
+	$scope.listaatalhos = [];
+	$scope.listalateral = [];
+	$scope.erro = false;
+	$scope.versao = "Versão prerelease";
+
+	$scope.listalateral = [{
+			"nome": "Atendimento",
+			"atalho": "http://10.32.97.205:8080/atendimento",
+			"imagem": "capa_atende.svg"
+		}, {
+			"nome": "Ponto Eletrônico",
+			"atalho": "http://www-sisref/",
+			"imagem": "capa_ponto.svg"
+		}, {
+			"nome": "Novidades",
+			"atalhos": "novidades.html",
+			"imagem": "capa_noticia.svg"
+		}];
+
+	$http.get("/data/atalhosprincipais.json").then(function(response) {
+		$scope.listaatalhos = response.data.atalhos;
+	}, function(error) {
+		$scope.erro = true;
+	});
+});
+
+app.controller('conhecimentoCtrl',  function($scope, $http) {
+	$http.get("/bc/categorias.json").then(function(response) {
+		$scope.dados = response.data;
+	}, function(error) {
+		alert(error.message);
+	});
+});
